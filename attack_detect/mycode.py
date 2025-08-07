ca10f6adab680584b8db227ec42b1429bbc55bee
ha5804 <zixpotf5803@gmail.com>
2025-08-07 16:56:53 +0900

    model.train

--- attack_detect/mycode.py
+++ attack_detect/mycode.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
-
+import numpy as np 
 class MyData:
     def __init__(self):
         self.train = None
@@ -42,10 +42,73 @@ y = data.binary_label()
 # print(y)
 print(y)
 
-class util:
+class Model:
     def __init__(self):
+        self.weight = np.zeros((84, 1))
         pass
 
-    def predinct(self, x, y):
+    def predict(self, x):
+        feature_matrix = x
+        z = feature_matrix @ self.weight
+        pred = self.activate(z)
+        return pred
+    
+    def activate(self, x):
+        y = 1/ (1+np.exp(-x))
+        return y
+    
+    def update(self, weight):
+        self.weight = weight
+    
+class MyOptim:
+    def __init__(self, model):
+        self.model  = model
+        self.eps    = 1e-8
+        self.lr = 0.1
+    def compute_loss(self, label, prediction):
+        # ===========================
+        eps = self.eps
+        loss = (1 / (len(label))) * ((((-1) * label.T) @ np.log(prediction + eps)) - ((np.ones_like(label) - label).T @ np.log(np.ones_like(label) - prediction + eps)))
+        # ===========================
+        return loss
+     
+    def compute_residual(self, label, prediction):
+        # ===========================
+        res = label - prediction
+        # ===========================
+        return res
+   
+    def compute_gradient(self, x, label, pred):
+        # ===========================
+        A = x
+        grad = (1 / (len(label))) * (A.T @ (pred - label))
+        # ===========================
+        return grad
         
+    def step(self, grad):
+        # ===========================
+        weight = self.model.weight
+        next_weight = weight - (self.lr * grad)
+        self.model.update(next_weight)
+        pass 
+        # ===========================
 
+
+
+def train(x, y):
+    model = Model()
+    optim = MyOptim(model)
+    loss_lis = []
+    y = y.values.reshape(-1,1)
+    for _ in range(100):
+        predict = model.predict(x)
+        loss = optim.compute_loss(y, predict)
+        grad = optim.compute_gradient(x, y, predict)
+        optim.step(grad)
+        if _ % 10 == 0:
+            loss_lis.append(loss)
+    return loss_lis
+
+ls = train(x, y)
+for i in ls:
+    print(i)
